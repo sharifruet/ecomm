0430c2b4f0ec64e9c985abadbc50972c0441fd17
Sharif Uddin <sharifruet@gmail.com>
2023-08-22 11:35:40 +0600

    bootstrap added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
-import { Container, Navbar, Nav, FormControl, InputGroup } from 'react-bootstrap';
+import { Container, Navbar, Nav, FormControl, InputGroup, NavDropdown } from 'react-bootstrap';
 import styles from '../styles/Header.module.css';
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg" className={styles.header}>
       <Container>
-        <Navbar.Brand>
-          <Link href="/">
-            <img
-              src="/images/wil-logo.png"
-              alt="Logo"
-              className={styles.logo}
-            />
-          </Link>
-          <h1 className={styles.siteName}>ওয়াহীদিয়া ইসলামিয়া লাইব্রেরী</h1>
+        <Navbar.Brand href="/">
+          <img src="/images/wil-logo.png" alt="Logo" className={styles.logo} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={styles.nav}>
-            <Link href="/">লাইব্রেরী</Link>
-            <Link href="/publisher">প্রকাশনা</Link>
-            <Link href="/free-ebook">ফ্রি ই-বুক</Link>
-            <Link href="/dawah">দাওয়াহ</Link>
+          <Nav className="me-auto">
+            <Nav.Link href="/">লাইব্রেরী</Nav.Link>
+            <Nav.Link href="/publications">প্রকাশনা</Nav.Link>
+            <Nav.Link href="/free-ebooks">ফ্রি ই-বুক</Nav.Link>
+            <Nav.Link href="/dawah">দাওয়াহ</Nav.Link>
           </Nav>
-          <InputGroup className={styles.search}>
-            <FormControl placeholder="Search" />
-          </InputGroup>
-          <Link href="/cart" className={styles.cart}>
-            Cart
-          </Link>
         </Navbar.Collapse>
       </Container>
+
     </Navbar>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
 import Footer from '../components/Footer';
+import '../styles/globals.css';
+
 
 function HomePage() {
   return (
