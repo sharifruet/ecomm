271d77994e4913cacada52262b428fb91588a4b5
Sharif Uddin <sharifruet@gmail.com>
2023-08-23 10:45:19 +0600

    nav updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
-import Link from 'next/link';
-import { Container, Navbar, Nav, FormControl, InputGroup, NavDropdown } from 'react-bootstrap';
+import { Container, Navbar, Nav, Form, FormControl, Row, Col } from 'react-bootstrap';
 import styles from '../styles/Header.module.css';
+import { Search, Cart } from 'react-bootstrap-icons';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" className={styles.header}>
-      <Container>
-        <Navbar.Brand href="/">
-          <img src="/images/wil-logo.png" alt="Logo" className={styles.logo} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">লাইব্রেরী</Nav.Link>
-            <Nav.Link href="/publications">প্রকাশনা</Nav.Link>
-            <Nav.Link href="/free-ebooks">ফ্রি ই-বুক</Nav.Link>
-            <Nav.Link href="/dawah">দাওয়াহ</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-
-    </Navbar>
+    <Container className={styles.header}>
+      <Row>
+        <Col sm={2}>
+          <div className={styles.logo}><img src="/images/wil-logo.png" alt="Logo" /></div>
+        </Col>
+        <Col sm={10}>
+          <h2>ওয়াহীদিয়া ইসলামিয়া লাইব্রেরী</h2>
+          <Navbar bg="light" expand="lg" >
+            <Container>
+                <Nav className={`mr-auto ${styles.nav}`}>
+                  <Nav.Link href="/">লাইব্রেরী</Nav.Link>
+                  <Nav.Link href="/publications">প্রকাশনা</Nav.Link>
+                  <Nav.Link href="/free-ebooks">ফ্রি ই-বুক</Nav.Link>
+                  <Nav.Link href="/dawah">দাওয়া</Nav.Link>
+                </Nav>
+                <Form className={styles.search}>
+                  <FormControl size="lg" type="text" placeholder="Search" />
+                  <Search className={styles.searchIcon} />
+                </Form>
+                <Cart className={styles.cart} />
+            </Container>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
